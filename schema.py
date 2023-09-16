4b0c8511b8077439f7519ac89a69e10cc32bbbc8
amir.ch.charehei1382 <amir.ch.charehei1382@gmail.com>
2023-09-16 16:48:43 +0330

    leave some comments in schema.py

--- schema.py
+++ schema.py
@@ -4,11 +4,11 @@ from pydantic import BaseModel, ConfigDict
 
 class ZipCode(BaseModel):
    code: str
-   # model_config = ConfigDict(from_attributes=True)
-
    class Config:
       orm_mode = True
 
 
+# create a read class for syncing with database fields and 
+# filter some importatnt fields (security issues!)
 class ZipCodeRead(ZipCode):
    id: int
